ed9b2530aca55d9abad5a2bf4714cfcea25243d8
VerCyd <144364760+VerCyd@users.noreply.github.com>
2023-10-10 22:05:06 +0300

    Final_Project

--- Main.py
+++ Main.py
@@ -7,27 +7,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-#Data taken from the website:
+# Data taken from the website:
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/117.0.0.0 Safari/537.36 OPR/102.0.0.0'
+                  'Chrome/117.0.0.0 Safari/537.36 OPR/103.0.0.0'
 }
-for i in range(1, 12):
-    url = "https://www.pamatyklietuvoje.lt/zemelapis?scf=1%2C2&xmax=283228&xmin=11.2720&ymax=" \
-          f"59.4339&ymin=49.8805&stf=37{i}"
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-    forecast = soup.find_all("div", class_ = "resultItem")
-    print(forecast)
-    PlacesToVisit = []
-    for places in forecast:
-        title = places.find("div", class_ = "detailsContainer").text.strip()
-        species = places.find("div", class_ = "resultItem_subTypeText").text.strip()
-        address = places.find("div", class_ = "resultItem_addressText").text.strip()
-        PlacesToVisit.append((title, species, address))
-    df = pd.DataFrame(PlacesToVisit, columns = ["Title", "Species", "Address"])
+url = "https://www.pamatyklietuvoje.lt/keliones/marsrutai-pesciomis/10"
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
+forecast = soup.find_all("div", class_="col-xs-12 col-sm-6 col-lg-4")
+print(forecast)
+RoutesOnFoot = []
+for routes in forecast:
+    title = routes.find_all("div", class_="trip-title").text.strip()
+    distance = routes.find("span", class_="pl pl-distance").text.strip()
+    duration = routes.find("span", class_="pl pl-duration").text.strip()
+    objects = routes.find("span", class_="pl pl-poi").text.strip()
+    RoutesOnFoot.append((title, distance, duration, objects))
+    df = pd.DataFrame(RoutesOnFoot, columns=["Title", "Distance", "Duration", "Objects"])
     print(df)
-
-
-
 
